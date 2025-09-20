536d9df8c07e7aad6c0ba7bcbec1211a5e0a41f6
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-20 12:43:03 -0700

    created socket, starting to make hint structure

--- TCP-Client/TCP-Client/main.cpp
+++ TCP-Client/TCP-Client/main.cpp
@@ -10,8 +10,23 @@ void main()
 	WORD ver = MAKEWORD(2, 2);
 	int wsResult = WSAStartup(ver, &data);
 
+	if (wsResult != 0)
+	{
+		std::cerr << "Can't start winsock, Err #" << wsResult << std::endl;
+		return;
+	}
+
 	//create the socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sock == INVALID_SOCKET)
+	{
+		std::cerr << "cannot create socket, Err #" << WSAGetLastError() << std::endl;
+	}
+
 	//fill hint structure
+	sockaddr_in hint;
+	 
 	//connect to serv
 	// do-while to receive and send data
 	//gracefully close down
--- TCP-Client/TCP-Client/tcp.h
+++ TCP-Client/TCP-Client/tcp.h
@@ -1,4 +1,5 @@
 #include <iostream>
 #include < ws2tcpip.h>
 #include <string>
+
 #pragma comment(lib, "ws2_32lib")

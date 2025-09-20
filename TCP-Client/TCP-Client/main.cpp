9eeb4b17b465fe8d859bf42d36ecee315c82f459
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-20 10:47:34 -0700

    init winsock

--- TCP-Client/TCP-Client/main.cpp
+++ TCP-Client/TCP-Client/main.cpp
@@ -2,5 +2,17 @@
 
 void main()
 {
+	std::string ipAddress = "127.0.0.1";
+	int port = 54000;	
 
+	//initialize winsock
+	WSADATA data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+
+	//create the socket
+	//fill hint structure
+	//connect to serv
+	// do-while to receive and send data
+	//gracefully close down
 }
